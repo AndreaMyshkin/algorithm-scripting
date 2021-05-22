26a0db7e6a82522d20de3fb16d022af01a9c2cfe
AndreaMyshkn <a.trkvsk@hotmail.com>
2021-05-22 01:30:49 -0500

    Alpha

---
+++ alpha.js
@@ -0,0 +1,12 @@
+
+// Sum All Numbers in a Range
+const sumAll =(arr) => {
+    const o= arr.sort((a, b) => a-b)
+    let a = []
+    for (let i = o[0]; i <= o[1]; i++){
+            a.push(i)   
+    }
+   return a.reduce((a, b) => a+b)
+  }
+  
+  sumAll([1, 4]);
